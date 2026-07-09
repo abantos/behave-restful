30c15bc7d350f9a9e9216dc4dd2a40d12fa10856
Ahmed-Elmoslmany <ahmedelmoslmany74@gmail.com>
2026-07-09 11:36:09 +0300

    request_invoker

--- tests/test_lang_imp/test_request_invoker.py
+++ tests/test_lang_imp/test_request_invoker.py
@@ -1,3 +1,5 @@
+import os
+import tempfile
 import unittest
 
 from assertpy import assert_that
@@ -136,6 +138,107 @@ class TestRequestInvokerInterface(unittest.TestCase):
         )
 
 
+class TestRequestInvokerFileUpload(unittest.TestCase):
+    def setUp(self):
+        super(TestRequestInvokerFileUpload, self).setUp()
+        self.response = ResponseDouble()
+        self.session = SessionDouble()
+        self.session.response = self.response
+        self.context = ContextDouble()
+        self.context.session = self.session
+        self.context.request_url = "http://my.server.com/resource"
+        self.context.request_params = {"foo": 56, "bar": "baz"}
+        self.context.request_headers = {"foo": 56, "bar": "baz"}
+        self.file_path = self._create_temp_file(suffix=".txt")
+        self.context.request_files = {"attachment": self.file_path}
+        self.context.request_form_data = {"description": "a file"}
+
+    def _create_temp_file(self, suffix):
+        handle, path = tempfile.mkstemp(suffix=suffix)
+        with os.fdopen(handle, "wb") as f: f.write(b"file content")
+        self.addCleanup(os.remove, path)
+        return path
+
+    def _sent_file(self, field="attachment"):
+        return self.session.request_files[field]
+
+    # send_post()
+    def test_invokes_post_on_session(self):
+        _invoker.send_post(self.context)
+        assert_that(self.session.post_invoked).is_true()
+        assert_that(self.session.request_url).is_equal_to(self.context.request_url)
+
+    def test_post_stores_response_in_context(self):
+        _invoker.send_post(self.context)
+        assert_that(self.context.response).is_same_as(self.response)
+
+    def test_post_request_includes_params(self):
+        _invoker.send_post(self.context)
+        assert_that(self.session.request_params).is_same_as(self.context.request_params)
+
+    def test_post_request_includes_headers(self):
+        _invoker.send_post(self.context)
+        assert_that(self.session.request_headers).is_same_as(self.context.request_headers)
+
+    def test_sends_file_with_name_and_content_type(self):
+        _invoker.send_post(self.context)
+        file_name, file_object, content_type = self._sent_file()
+        assert_that(file_name).is_equal_to(os.path.basename(self.file_path))
+        assert_that(file_object.name).is_equal_to(self.file_path)
+        assert_that(content_type).is_equal_to("text/plain")
+
+    def test_defaults_content_type_when_it_cannot_be_guessed(self):
+        path = self._create_temp_file(suffix=".unknown")
+        self.context.request_files = {"attachment": path}
+        _invoker.send_post(self.context)
+        _, _, content_type = self._sent_file()
+        assert_that(content_type).is_equal_to("application/octet-stream")
+
+    def test_sends_form_data_with_files(self):
+        _invoker.send_post(self.context)
+        assert_that(self.session.request_data).is_same_as(self.context.request_form_data)
+
+    def test_form_data_is_optional(self):
+        del self.context.request_form_data
+        _invoker.send_post(self.context)
+        assert_that(self.session.request_data).is_none()
+
+    def test_does_not_send_json_payload_with_files(self):
+        _invoker.send_post(self.context)
+        assert_that(self.session.request_json).is_none()
+
+    def test_closes_files_after_request(self):
+        _invoker.send_post(self.context)
+        _, file_object, _ = self._sent_file()
+        assert_that(file_object.closed).is_true()
+
+    # send_put()
+    def test_invokes_put_with_files(self):
+        _invoker.send_put(self.context)
+        assert_that(self.session.put_invoked).is_true()
+        file_name, _, content_type = self._sent_file()
+        assert_that(file_name).is_equal_to(os.path.basename(self.file_path))
+        assert_that(content_type).is_equal_to("text/plain")
+        assert_that(self.session.request_data).is_same_as(self.context.request_form_data)
+
+    def test_put_stores_response_in_context(self):
+        _invoker.send_put(self.context)
+        assert_that(self.context.response).is_same_as(self.response)
+
+    # send_patch()
+    def test_invokes_patch_with_files(self):
+        _invoker.send_patch(self.context)
+        assert_that(self.session.patch_invoked).is_true()
+        file_name, _, content_type = self._sent_file()
+        assert_that(file_name).is_equal_to(os.path.basename(self.file_path))
+        assert_that(content_type).is_equal_to("text/plain")
+        assert_that(self.session.request_data).is_same_as(self.context.request_form_data)
+
+    def test_patch_stores_response_in_context(self):
+        _invoker.send_patch(self.context)
+        assert_that(self.context.response).is_same_as(self.response)
+
+
 class SessionDouble(object):
     def __init__(self):
         self.response = None
@@ -149,6 +252,7 @@ class SessionDouble(object):
         self.request_params = None
         self.request_data = None
         self.request_json = None
+        self.request_files = None
         self.request_kwargs = None
 
     def get(self, url, params=None, headers=None, **kwargs):
@@ -159,32 +263,35 @@ class SessionDouble(object):
         self.request_kwargs = kwargs
         return self.response
 
-    def post(self, url, data=None, json=None, params=None, headers=None, **kwargs):
+    def post(self, url, data=None, json=None, params=None, headers=None, files=None, **kwargs):
         self.post_invoked = True
         self.request_url = url
         self.request_headers = headers
         self.request_data = data
         self.request_json = json
         self.request_params = params
+        self.request_files = files
         self.request_kwargs = kwargs
         return self.response
 
-    def put(self, url, data=None, params=None, headers=None, **kwargs):
+    def put(self, url, data=None, params=None, headers=None, files=None, **kwargs):
         self.put_invoked = True
         self.request_url = url
         self.request_headers = headers
         self.request_data = data
         self.request_params = params
+        self.request_files = files
         self.request_kwargs = kwargs
         return self.response
 
-    def patch(self, url, data=None, json=None, params=None, headers=None, **kwargs):
+    def patch(self, url, data=None, json=None, params=None, headers=None, files=None, **kwargs):
         self.patch_invoked = True
         self.request_url = url
         self.request_headers = headers
         self.request_data = data
         self.request_json = json
         self.request_params = params
+        self.request_files = files
         self.request_kwargs = kwargs
         return self.response
 
